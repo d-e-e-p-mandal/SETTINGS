bcffa2f01a5e788499054d7e7a0aa68e3f01e87b
Deep Mandal <deepmandal9091@gmail.com>
2024-12-29 00:01:50 +0530

    Update q001_structure_basic_1.c

--- STRUCTURE/q001_structure_basic_1.c
+++ STRUCTURE/q001_structure_basic_1.c
@@ -1,8 +1,9 @@
+// Basic stracture - Input & Output
 #include<stdio.h>
-struct{
+struct student{
 	int rn ;
 	char name[20];
-	char clas[10];
+	char cl[10];
 	
 }s1;
 int main(){
@@ -11,8 +12,20 @@ int main(){
     printf("Enter name : ");
 	scanf("%s",s1.name);
 	printf("Enter class : ");
-	scanf("%s",s1.clas);
-	printf("You entered %d %s %s\n",s1.rn ,s1.name ,s1.clas );
+	scanf("%s",s1.cl);
+	printf("You entered roll no %d\n",s1.rn);
+	printf("You entered name %s\n",s1.name);
+	printf("You entered class %s\n",s1.cl );
     return 0;
 
 }
+/*
+Output :
+
+Enter roll number : 21
+Enter name : Deep
+Enter class : Mca
+You entered roll no 21
+You entered name Deep
+You entered class Mca
+*/
